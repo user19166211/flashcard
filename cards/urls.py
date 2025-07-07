b96cf568509666586287f132931fa74b01624127
user19166211 <19166211@brookes.ac.uk>
2025-07-07 13:06:53 +0900

    tag view

--- cards/urls.py
+++ cards/urls.py
@@ -7,6 +7,21 @@ urlpatterns = [
     path(
         "",
         views.CardListView.as_view(),
-        name="card_list"
+        name="card-list"
+    ),
+    path(
+        "new",
+        views.CardCreateView.as_view(),
+        name="card-create"
+    ),
+    path(
+        "edit/<int:pk>",
+        views.CardUpdateView.as_view(),
+        name="card-update"
+    ),
+    path(
+        "box/<int:box_num>",
+        views.BoxView.as_view(),
+        name="box"
     ),
 ]
