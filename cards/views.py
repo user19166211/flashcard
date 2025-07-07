dc585ed16b0253757eae87716e88be0de0cb0b35
user19166211 <19166211@brookes.ac.uk>
2025-07-07 13:13:55 +0900

    correct/incorrect buttons updates

--- cards/views.py
+++ cards/views.py
@@ -8,6 +8,10 @@ from django.views.generic import (
 )
 
 from .models import Card
+import random
+from django.shortcuts import get_object_or_404, redirect
+from .forms import CardCheckForm
+
 
 class CardListView(ListView):
     model = Card
@@ -23,6 +27,7 @@ class CardUpdateView(CardCreateView, UpdateView):
 
 class BoxView(CardListView):
     template_name = "cards/box.html"
+    form_class = CardCheckForm
 
     def get_queryset(self):
         return Card.objects.filter(box=self.kwargs["box_num"])
@@ -30,4 +35,13 @@ class BoxView(CardListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["box_number"] = self.kwargs["box_num"]
+        if self.object_list:
+            context["check_card"] = random.choice(self.object_list)
         return context
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            card = get_object_or_404(Card, id=form.cleaned_data["card_id"])
+            card.move(form.cleaned_data["solved"])
+
+        return redirect(request.META.get("HTTP_REFERER"))
